ff8a2f709ce5bf5631bec579e9332d0df1a97acf
Milldew <107108504+Drakelid@users.noreply.github.com>
2026-03-26 06:52:11 +0100

    docker fix

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,20 @@ const PUBLIC_PATH_PREFIX = "/portfolio";
 
 const nextConfig: NextConfig = {
   assetPrefix: process.env.NODE_ENV === "production" ? PUBLIC_PATH_PREFIX : undefined,
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: PUBLIC_PATH_PREFIX,
+          destination: "/",
+        },
+        {
+          source: `${PUBLIC_PATH_PREFIX}/:path*`,
+          destination: "/:path*",
+        },
+      ],
+    };
+  },
   cacheComponents: true,
   turbopack: {
     root: path.resolve(__dirname),
